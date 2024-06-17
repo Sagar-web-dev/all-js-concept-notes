a1585f48f14aef896c40501378c8e1267c6e0a8c
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-17 15:04:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"  // treat all js code as newer version 
+
+
+
+// alert(3+3)   // we are using nodejs , not browser
+
+// console.log(3
+//     +3);  // code redability must be high
+
+//     console.log("hitesh");
+
+
+    let name = "hitesh";
+    let age = 18;
+    let isLoggedIN = false;
+
+
+    // datatypes
+// number == 2  to power 53
+// BigInt =>  
+//     string =="",''  preffered double quotation
+// boolean == true/false
+// null == standalone value
+// undefined  == abhi value assign nhi hui hai
+// symbol = unique   all are primitive datatye
+
+
+// object 
+console.log(typeof "hitesh");
+console.log(typeof null);    // iska type hai object
+console.log(typeof undefined); // iska type hai undefined
+
+
