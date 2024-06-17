595078e8ad9682a184495604e07f92409a654317
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-17 16:06:23 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = 33;  // test case null undefined "33abc"  true/false "sagar"
+
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInnumber = Number(score);
+
+// console.log(valueInnumber);
+// console.log( typeof valueInnumber);
+
+
+//33 = number
+// "33asdfg" = Nan not a number
+//true = 1 false = 0
+
+let isLoggedIN = 1;
+
+let booleanISloggedin = Boolean(isLoggedIN);
+
+console.log( booleanISloggedin);
+
+
+// 1 = true 0 = false;
+// "" == false
+//"sagar"  == true
+
+
+
+let someNUmber  = 88;
+
+let stringNumber = String(someNUmber);
+console.log(stringNumber);
+console.log( typeof  stringNumber);
