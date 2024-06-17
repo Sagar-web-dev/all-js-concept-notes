bf27993d45d1cd33979dfa533a01379afda0020c
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-17 18:25:33 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,7 @@ let isLoggedIN = 1;
 
 let booleanISloggedin = Boolean(isLoggedIN);
 
-console.log( booleanISloggedin);
+// console.log( booleanISloggedin);
 
 
 // 1 = true 0 = false;
@@ -29,6 +29,51 @@ console.log( booleanISloggedin);
 
 let someNUmber  = 88;
 
-let stringNumber = String(someNUmber);
-console.log(stringNumber);
-console.log( typeof  stringNumber);
+// let stringNumber = String(someNUmber);
+// console.log(stringNumber);
+// console.log( typeof  stringNumber);
+
+// +++++++++++++++++++++++++++++++++ operation ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let value = 9;
+
+let negValue = -value;
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2+2);
+
+
+let str = "Sagar";
+let str1 = " Rahangdale";
+
+let mainStr = str + str1;
+// console.log(mainStr);
+
+
+
+// console.log("1" + 2);
+// console.log("1" + 2 +4);
+// console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log( +"");
+
+
+let num1 , num2 , num3;
+
+num1 = num2 = num3 =2+2;
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
+
+
+// converstion based on the preferences
+
