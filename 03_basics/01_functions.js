3b6e2aebd04e5eeda45852ee5a91ebb169e9ce7c
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-26 08:48:42 +0000

    access the value of object , array in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -55,9 +55,51 @@ function logInuser(username){
 
 // console.log(logInuser("hitesh"));
 // console.log(logInuser(""));
-console.log(logInuser());   // it give us undefined so for that we use the if else condition
+// console.log(logInuser());   // it give us undefined so for that we use the if else condition
 
 
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++===new video ++++++++++++++++++++++++++++++++++++++++++++++
+
+                             //... is now act as the rest operator  it help to collect all the parameter and give us the array
+                             // spread and rest operatot are the same but differ from the functionality
+function calculateCartPrice(val1,val2 , ...num1) {
+       return num1
+}
+
+// console.log(calculateCartPrice(23,45,88,99,66,55));
+
+
+const user ={
+    username:"Sagar",
+    price:"999"
+}
+
+function handleObject(anyObject) {
+         console.log(`Username is ${anyObject.username} and Price is ${anyObject.price}`);
+}
+
+// handleObject(user);   // ist method
+// handleObject({
+//     username:"ram",
+//     price:"2345"
+// })    // in this way we also pass the object under the function or access the object
+
+
+
+// console.log(typeof handleObject);
+
+
+
+
+
+const newArray = [200,300,400,600];
+
+function returnSecondvalue(getarray){
+        return getarray[1];
+}
+
+// console.log(returnSecondvalue(newArray));   // 1st method
+
+console.log(returnSecondvalue([200,3000,88,777]));
