70114be09a9f9c1e0777a0bd40946c36b781e814
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-27 17:32:54 +0000

    truthy

--- 05_controlflow.js/truthy.js
+++ 05_controlflow.js/truthy.js
@@ -38,6 +38,35 @@ if (Object.keys(emptyObj).length === 0) {
 
 
 
+// Nullish Coalishing Operator (??) : null undefined
+// it specially design for the nul and unndefined
+
+let val1;
+// val1 = 6??9;
+// val1 = null ?? 77;
+// val1 = null ?? undefined;
+// val1 = undefined ?? 5678;
+val1 = null ?? 10??66;   // here it take 1st value afetr the null
+
+
+
+
+console.log(val1);
+
+
+// ------------------------------------------------------------
+
+// ternary operator
+
+// condition ? true : false
+
+const iceTeaprice = 100;
+
+iceTeaprice <= 80 ?  console.log("less than 80"): console.log("more than 80");
+
+
+
+
 
 
 
