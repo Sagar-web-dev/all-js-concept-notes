96e2c4ed1cc729410ea4ff297526320aa868dc3e
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-27 12:27:40 +0000

    switch and truthy

---
+++ 05_controlflow.js/switch.js
@@ -0,0 +1,64 @@
+// basic sentence
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+
+//     default:
+//         console.log("default case");
+//         break;
+// }
+
+// ------------------------------------------------------------------
+// in case of diifeerent  datatypes
+
+const month = "april";
+
+// switch (month) {
+//     case "jan":
+//         console.log("jan");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+//     case "may":
+//         console.log("may");
+//         break;
+
+//     default:
+
+//     console.log("default case");
+//         break;     // break is neccessary to break the loop if break is not there so the loop run till the end except default
+// }
+
+// ----------------------------------------------------------------------------
---
+++ 05_controlflow.js/truthy.js
@@ -0,0 +1,45 @@
+let username = "sagar";
+
+// if (username) {
+//     console.log("username availble");
+// } else {
+//     console.log("username not available");
+// }
+
+
+//  falsy values
+
+// false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+
+//truthy values
+
+// "0" , 'false', "" ,  [],{}, function(){}
+
+
+// for checking
+
+
+let name = [];
+if (name.length === 0) {
+    console.log("Array is empty");
+};
+
+const emptyObj = {};
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("obj is empty");
+    
+}
+
+
+
+// ---------------------------------------- pause 7.000
+
+
+
+
+
+
+
+
