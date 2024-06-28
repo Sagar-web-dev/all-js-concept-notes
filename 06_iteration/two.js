4eee3b05b32b5e9e619a7c74da36a2dd20b5f893
Sagar-web-dev <136608754+Sagar-web-dev@users.noreply.github.com>
2024-06-28 07:54:09 +0000

    while and do while

---
+++ 06_iteration/two.js
@@ -0,0 +1,32 @@
+
+
+
+let index = 1;
+
+// while (index <=10) {
+//     console.log(`Value of the index ${index}`);
+
+//     index = index + 2;
+// }
+
+let myArray =["sagar", "cp","aman", "paras"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`value of myarray: ${myArray[arr]} my roommates`);
+    arr = arr + 1;
+}
+
+
+
+// -----------------------------------------------------------------------------
+
+
+// do while loop me phele kaam hota hai than condition check hoti hai
+
+let score = 1;
+
+do {
+    console.log(`Score: ${score}`);
+    score++
+} while (score <=10);
